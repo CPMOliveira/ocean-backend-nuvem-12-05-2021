e7706f4e58de41d493f14265910eda5d56faca90
Cristiano Oliveira <cpmoliveira@gmail.com>
2021-05-12 12:18:57 -0300

    Mongo Cloud

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 (async () => {
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://admin:PRsAPapRTr3ze3in@cluster0.f7g6e.mongodb.net/ocean_db?retryWrites=true&w=majority';
 
-const dbName = 'ocean_bancodados_11_05_2021';
+const dbName = 'ocean_db';
 
 console.info('Conectando ao banco de dados...');
 
@@ -23,8 +23,6 @@ app.get('/hello', function (req, res) {
   res.send('Hello World');
 });
 
-const mensagens = ['Essa é a primeira mensagem!', 'Essa é a segunda mensagem!'];
-
 const mensagensCollection = db.collection('mensagens');
 
 // CRUD (Create, Read, Update, Delete)
@@ -37,45 +35,48 @@ app.get('/mensagens', async (req, res) => {
 });
 
 // GET: READ SINGLE (exibir apenas um registro)
-app.get('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+app.get('/mensagens/:id', async (req, res) => {
+  const id = req.params.id;
 
-  const mensagem = mensagens[id];
+  const mensagem = await mensagensCollection.findOne({ _id: ObjectId(id) });
 
   if (!mensagem) {
     res.send('Mensagem não encontrada.');
+    
+    return;
   }
 
   res.send(mensagem);
 });
 
 // POST: CREATE (criar um registro)
-app.post('/mensagens', (req, res) => {
-  const mensagem = req.body.mensagem;
+app.post('/mensagens', async (req, res) => {
+  const mensagem = req.body;
 
-  mensagens.push(mensagem);
+  await mensagensCollection.insertOne(mensagem);
 
-  const id = mensagens.length;
-
-  res.send(`Mensagem '${id}' criada com sucesso.`);
+  res.send(mensagem);
 });
 
 // PUT: UPDATE (editar um registro)
-app.put('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+app.put('/mensagens/:id', async (req, res) => {
+  const id = req.params.id;
 
-  const mensagem = req.body.mensagem;
+  const mensagem = req.body;
 
-  mensagens[id] = mensagem;
+  await mensagensCollection.updateOne(
+    { _id: ObjectId(id) },
+    { $set: mensagem }
+  );
 
-  res.send('Mensagem atualizada com sucesso.');
+  res.send('Mensagem editada com sucesso.');
 });
 
 // DELETE: DELETE (remover um registro)
-app.delete('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+app.delete('/mensagens/:id', async (req, res) => {
+  const id = req.params.id;
 
-  delete mensagens[id];
+  await mensagensCollection.deleteOne({ _id: ObjectId(id) });
 
   res.send('Mensagem removida com sucesso.');
 });
